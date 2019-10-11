bbf7da4594562b88fc2db5a3c8fd3f5bce1bbf18
Þórður Atlason <thorduratl17@ru.is>
2019-10-11 16:06:59 +0900

    Logging

--- P3_GROUP_75.cpp
+++ P3_GROUP_75.cpp
@@ -632,7 +632,10 @@ int main(int argc, char *argv[])
 
     // connect to botnet, maybe change maxfds, add to open_sockets, add to botnet_servers
     if( connect_to_botnet_server(botnet_server_ip, botnet_server_port, botnet_servers, maxfds, open_sockets) < 0){
+        if_verbose("Connected to botnet server");
         send_through_socket(clientSock, "Connected to indicated server");
+    }else{
+        if_verbose("Did not connect to server");
     }
 
     auto start = chrono::high_resolution_clock::now();
